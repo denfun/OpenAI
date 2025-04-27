8ca684f3deabc175a22a7ba5d59535a942140c33
DF <835891284@qq.com>
2025-04-27 10:59:45 +0800

    修改systemFingerprint

--- Sources/OpenAI/Public/Models/ChatStreamResult.swift
+++ Sources/OpenAI/Public/Models/ChatStreamResult.swift
@@ -179,7 +179,7 @@ public struct ChatStreamResult: Codable, Equatable, Sendable {
     /// Can be more than one if `n` is greater than 1.
     public let choices: [Choice]
     /// This fingerprint represents the backend configuration that the model runs with. Can be used in conjunction with the `seed` request parameter to understand when backend changes have been made that might impact determinism.
-    public let systemFingerprint: String
+    public let systemFingerprint: String?
     /// Usage statistics for the completion request.
     public let usage: ChatResult.CompletionUsage?
 
