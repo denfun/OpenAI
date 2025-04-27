1087d47acd692e5937f2cbdbbd93ed3ab066f29f
DF <835891284@qq.com>
2025-04-27 15:43:11 +0800

    1

--- Sources/OpenAI/Private/Streaming/StreamingSession.swift
+++ Sources/OpenAI/Private/Streaming/StreamingSession.swift
@@ -101,7 +101,8 @@ final class StreamingSession<Interpreter: StreamInterpreter>: NSObject, Identifi
             self.onReceiveContent?(self, content)
         } onError: { [weak self] error in
             guard let self else { return }
-            self.onProcessingError?(self, error)
+            //self.onProcessingError?(self, error)
+            self.onReceiveContent?(self, content)
         }
     }
 }
